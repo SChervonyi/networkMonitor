7ee4551c3ffcf584499681a76c66a1e9dd9381dd
Sergii Chervonyi <bekyts@gmail.com>
2018-02-10 15:52:42 +0200

    Implement application for .net framework

---
+++ PosNetworkMonitor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PosNetworkMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var currentIp = Dns.GetHostAddresses(Dns.GetHostName())
+                           .First(address => address.AddressFamily == AddressFamily.InterNetwork);
+            var printModelBuilder = new PrintModelBuilder();
+
+            var udp = new UDPer(currentIp, printModelBuilder);
+            udp.Start();
+
+            do
+            {
+                // udp.Send(GetDataSyncMessage(currentIp));
+                Thread.Sleep(2000);
+            }
+            while (true);
+        }
+
+        private static string GetDataSyncMessage(IPAddress currentIp)
+        {
+            string secret = "CHES";
+
+            return secret + "{\"meta\":{\"name\":\"CHES:f856\",\"sender\":{\"port\":12000,\"address\":\"" + currentIp.ToString() + "\"},\"channel\":\"XPOS:5a6efeffd282a5001feb665d\"}," +
+                "\"payload\":{\"feed\":\"orderState\",\"count\":7,\"updateTime\":1518170048779}}";
+        }
+    }
+}
